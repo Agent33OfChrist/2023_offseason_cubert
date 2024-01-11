3abd09a13ab6a8926b3dcbd399f283b6be02c7b6
Agent33OfChrist <artandbirds@gmail.com>
2024-01-11 17:58:15 -0500

    swapping names

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -50,15 +50,22 @@ public final class Constants
     public static final double LEFT_Y_DEADBAND = 0.01;
   }
 
-  public static final class Arm
+  public static final class Intake
     {
-        public static final int armMasterMoter = 15;
-        public static final int armSlaveMotor = 16;
+        public static final int intakeMasterMoter = 15;
+        public static final int intakeSlaveMotor = 16;
         
     }
 
     public static final class Shooter
     {
-        public static final int shooterMotorID = 9;
+        public static final int shooterMaster = 17;
+        public static final int shooterSlave = 18;
     }
+
+    public static final class Climber
+    {
+      public static final int climberMotorID = 9;
+    }
+
 }
